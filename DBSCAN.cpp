b0028283dd72e2e64a199bc61196e7f3ef83877f
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2024-04-09 23:40:10 +0200

    Adapt DBSCAN for multiple espilons sets

--- DBSCAN.cpp
+++ DBSCAN.cpp
@@ -1,4 +1,7 @@
 #include "DBSCAN.h"
+#include "ClusterInfo.h"
+#include "Characterization.h"
+
 float epsilonX = 50;
 float epsilonY = 50;
 float epsilonZ = 50;
@@ -10,29 +13,48 @@ float epsilonZ = 50;
 std::map<int, std::vector<Point>> detect_objects_clustering(Point points[])
 {
 
-    // Calculate k-distance for each point, Calculate espilon
-    calculateKDistance_set_Epsilon(points, epsilonX, epsilonY, epsilonZ);
-
-    if (numPoints > 0)
-    {
-        // Apply DBSCAN for clustering only on valid points
-        DBSCAN(points);
-    }
-    else
-    {
-        Serial.println("No valid data points collected.");
-    }
+    std::vector<std::vector<float>> Epsilons;
+    Epsilons = calculateKDistance_set_Epsilon(points, epsilonX, epsilonY, epsilonZ);
     std::map<int, std::vector<Point>> clusterMap;
-
-    // Populate cluster map with points
-    for (int i = 0; i < numPoints; ++i)
+    int clusterId = 0; // shared between all DBSCAN run so it can be incremented over esplilon sets
+    for (size_t i = 0; i < Epsilons.size(); ++i)
     {
-        int clusterId = points[i].clusterId;
-        if (clusterId > 0)
-        { // Assuming clusterId <= 0 are noise or unclassified
-            clusterMap[clusterId].push_back(points[i]);
+        // Accessing the i-th element of each epsilon vector
+        epsilonX = Epsilons[i][0];
+        epsilonY = Epsilons[i][1];
+        epsilonZ = Epsilons[i][2];
+        Serial.print(" epsilonX: ");
+        Serial.print(epsilonX);
+        Serial.print("   epsilonY: ");
+        Serial.print(epsilonY);
+        Serial.print("  epsilonZ,");
+        Serial.println(epsilonZ);
+
+        if (numPoints > 0)
+        {
+            // The Noise point from one set of Epsilon became a unclassified to be used so they can be clustered by next set
+            for (int j = 0; j < numPoints; ++j)
+            {
+                if (points[j].clusterId == NOISE)
+                    points[j].clusterId = UNCLASSIFIED;
+            }
+            // Apply DBSCAN for clustering only on valid points
+            DBSCAN(points, clusterId);
+        }
+        else
+        {
+            Serial.println("No valid data points collected.");
         }
     }
+        // Populate cluster map with points (it should be done only after all clusters are formed or it will have same repeated cluster after each iteration)
+        for (int j = 0; j < numPoints; ++j)
+        {
+            int clusterId = points[j].clusterId;
+            if (clusterId > 0)
+            {
+                clusterMap[clusterId].push_back(points[j]);
+            }
+        }
     return clusterMap;
 }
 
@@ -45,6 +67,29 @@ bool arePointsNeighbors(const Point &p1, const Point &p2)
 {
 
     return abs(p1.x - p2.x) <= epsilonX && abs(p1.y - p2.y) <= epsilonY && abs(p1.z - p2.z) <= epsilonZ;
+}
+
+/*
+The reason this function use (points[i].clusterId == NOISE || points[i].clusterId == UNCLASSIFIED )
+Suppose first set of epsilon(x,y,z) built a cluster
+Then a nother set of esplilon start searching then it will start to dins the core point  see DBSCAN
+Now finding core point will use getNeighbors and based on the size the core poitn found if size > minpoint
+But the problem if check all points there is possibility to creat a core poitn with another points were already classified in another cluster
+and that is not right.
+The solution is to search with the new set for the points that are not classefied or consider noise whcih means form a clusters from rest of points,
+and notice Noise should still be considered since many cores can be found with one set of espsilon, so some points are noise for one but are included into another
+*/
+
+void getNeighbors_multi_espsilon(Point points[], int pointIndex, std::vector<int> &neighbors)
+{
+    neighbors.clear();
+    for (int i = 0; i < numPoints; ++i)
+    {
+        if (arePointsNeighbors(points[pointIndex], points[i]) && (points[i].clusterId == NOISE || points[i].clusterId == UNCLASSIFIED ) )
+        {
+            neighbors.push_back(i);
+        }
+    }
 }
 
 void getNeighbors(Point points[], int pointIndex, std::vector<int> &neighbors)
@@ -71,7 +116,7 @@ void expandCluster(Point points[], int pointIndex, int clusterId, std::vector<in
             points[currNeighbor].clusterId = clusterId;
 
             std::vector<int> newNeighbors;
-            getNeighbors(points, currNeighbor, newNeighbors);
+            getNeighbors_multi_espsilon(points, currNeighbor, newNeighbors);
 
             for (int newNeighbor : newNeighbors)
             {
@@ -85,27 +130,28 @@ void expandCluster(Point points[], int pointIndex, int clusterId, std::vector<in
     }
 }
 
-void DBSCAN(Point points[])
+void DBSCAN(Point points[], int &clusterId)
 {
-    int clusterId = 0;
     for (int i = 0; i < numPoints; ++i)
     {
-        if (points[i].distance == 0 || points[i].distance == Limit_distance)
+        if (points[i].distance == 0 || points[i].distance == Limit_distance || points[i].clusterId != UNCLASSIFIED)
         {
-            continue;
+            continue; // Skip if the point doesn't meet the criteria or already classified
         }
-        if (points[i].clusterId == UNCLASSIFIED)
+        // Find core point
+        std::vector<int> neighbors;
+        getNeighbors_multi_espsilon(points, i, neighbors);
+
+        if (neighbors.size() < minPoints)
         {
-            std::vector<int> neighbors;
-            getNeighbors(points, i, neighbors);
-            if (neighbors.size() < minPoints)
-            {
-                points[i].clusterId = NOISE;
-            }
-            else
-            {
-                expandCluster(points, i, ++clusterId, neighbors);
-            }
+            points[i].clusterId = NOISE;
+        }
+        else
+        {
+
+            clusterId++; // Found a new core point, increment clusterId for a new cluster
+            points[i].clusterId = clusterId;
+            expandCluster(points, i, clusterId, neighbors);
         }
     }
 }
